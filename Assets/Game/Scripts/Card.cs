b794b680b9448bdeed5d7bf91bd86e1c2422e091
DiegoLlaury <157523047+DiegoLlaury@users.noreply.github.com>
2025-02-26 09:37:16 +0100

    CardData/CardScript

---
+++ Assets/Game/Scripts/Card.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WS_DiegoCo
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public List<CardType> cardType;
+        public bool corruption;
+        public int health;
+        public int damageMin;
+        public int damageMax;
+        public List<DamageType> damageType;
+        
+        public enum CardType
+        {
+            Fire,
+            
+            Earth,
+           
+            Water,
+            
+            Air,
+
+            Dark,
+
+            Light
+        }
+
+        public enum DamageType
+        {
+            Fire,
+
+            Earth,
+
+            Water,
+
+            Air,
+
+            Dark,
+
+            Light
+        }
+    }
+}
